61b5ae38fff6b01339a9b4cc982cc58bbcd3bd24
Baptiste SALVI <baptiste@onepilot.co>
2023-12-12 14:28:02 +0100

    Add users availabilities endpoint

---
+++ src/AircallAvailabilities.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Aircall;
+
+/**
+ * Class AircallAvailabilities.
+ *
+ * @see https://developer.aircall.io/api-references/#retrieve-list-of-users-availability
+ */
+class AircallAvailabilities extends AircallBase
+{
+    protected static string $baseEndpoint = 'availabilities';
+}
